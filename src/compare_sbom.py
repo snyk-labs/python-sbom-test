76f788610ef2fdc08fbe5107ccffacad5129f50c
samsnyk <samuel.dahlberg@snyk.io>
2024-08-05 13:23:59 -0400

    Update comparison to either list out found vulns, or print existing, new and fixed vulns

--- src/compare_sbom.py
+++ src/compare_sbom.py
@@ -5,14 +5,77 @@ class CompareSbom():
     def __init__(self):
         pass
 
-    def compare(self, file_a: str, file_b: str):
-        with open(file_a, 'r') as input:
+    def compare(self, file_a: str, file_b: str = ""):
+        input_vulns = self.load_sbom_vulns(file_a)
+
+        if file_b == "":
+            print("Identified vulns:")
+            for vuln in input_vulns:
+                print (f" - {vuln}")
+
+            ret_obj = {
+                "identified_vulns": input_vulns
+            }
+
+            return ret_obj
+        
+        else:
+            output_vulns = self.load_sbom_vulns(file_b)
+
+            fixed_vulns = []
+            existing_vulns = []
+            new_vulns = []
+
+            for vuln in output_vulns:
+                if vuln in input_vulns:
+                    existing_vulns.append(vuln)
+                else:
+                    new_vulns.append(vuln)
+
+            for vuln in input_vulns:
+                if vuln not in output_vulns:
+                    fixed_vulns.append(vuln)
+
+            print("Existing vulns:")
+            for vuln in existing_vulns:
+                print(f" - {vuln}")
+
+            print("")
+
+            print("New vulns:")
+            for vuln in new_vulns:
+                print(f" - {vuln}")
+            
+            print("")
+
+            print("Fixed vulns:")
+            for vuln in fixed_vulns:
+                print(f" - {vuln}")
+            
+            print("")
+
+            ret_obj = {
+                "existing_vulns": existing_vulns,
+                "new_vulns": new_vulns,
+                "fixed_vulns": fixed_vulns
+            }
+
+            return ret_obj
+
+    def load_sbom_vulns(self, file: str):
+        with open(file, 'r') as input:
             input_json = json.load(input)
 
-            with open(file_b, 'r') as output:
-                output_json = json.load(output)
+            data = input_json.get("data")
+            relationships = data.get("relationships")
+            vulns = relationships.get("vulnerabilities").get("data")
+            
+            vuln_ids = []
 
-                print(jsondiff.diff(input_json, output_json))
+            for vuln in vulns:
+                vuln_ids.append(vuln.get("id"))
+            
+            return vuln_ids
 
 
         
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import typer
 import json
+import os
 
 from sbom_test_api import SbomTestApi
 from compare_sbom import CompareSbom
@@ -8,28 +9,28 @@ app = typer.Typer()
 
 
 @app.command()
-def run(input_file: str, output_file: str):
+def run(input_sbom: str, output_sbom: str, orig_sbom: str = ""):
     print(f"Running application")
 
-    print(f"Input:  {input_file}")
-    print(f"Output: {output_file}")    
+    print(f"Input:  {input_sbom}")
+    print(f"Output: {orig_sbom}")    
 
-    api_token = ""
-    org_id = ""
+    api_token = os.environ["SNYK_TOKEN"]
+    org_id = os.environ["SNYK_ORG_ID"]
 
     sbom_api = SbomTestApi(api_token=api_token)
 
-    with open(input_file, 'r') as input:
+    with open(input_sbom, 'r') as input:
         data = json.load(input)
 
         sbom_test_result = sbom_api.run_sbom_test(org_id, data)
 
-        with open(output_file, 'w+') as output:
+        with open(output_sbom, 'w+') as output:
             json.dump(sbom_test_result, output)
 
     compare = CompareSbom()
 
-    compare.compare(input_file, output_file)
+    compare.compare(output_sbom, orig_sbom)
 
 if __name__ == "__main__":
     app()
